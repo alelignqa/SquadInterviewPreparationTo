01eca4a002373a4b1eb152064d26d68904e25887
Simar005 <letslive818@gmail.com>
2021-04-13 16:21:00 -0500

    increaseing and decreasing and max problems

---
+++ JavaPractice/src/easylevel/max/Simar.java
@@ -0,0 +1,30 @@
+package easylevel.max;
+
+public class Simar {
+
+
+    public static void main(String[] args) {
+
+       Simar.maxTwoNum();
+
+    }
+
+    public static void maxTwoNum(){
+
+
+        int a = 10;
+        int b = 25;
+
+
+        if (a<b){
+
+            System.out.println(a +"is the max number");
+        }
+        else if (b<a){
+            System.out.println(b+ "is the max number");
+        }
+    }
+
+
+
+}
