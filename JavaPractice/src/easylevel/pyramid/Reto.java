91e61aab409f8c6a1b978a242571dedf9100f4c9
isratreto4395 <isratreto4395@gmail.com>
2021-04-14 11:12:17 -0500

    pyramid

--- JavaPractice/src/easylevel/pyramid/Reto.java
+++ JavaPractice/src/easylevel/pyramid/Reto.java
@@ -1,4 +1,33 @@
 package easylevel.pyramid;
 
 public class Reto {
+
+    public static void main(String[] args) {
+        Reto.makeHalfAPyramid(6);
+        Reto.makeAFullPyramid(7);
+    }
+
+    public static void makeHalfAPyramid(int num) {// completed this in 10 minute
+        for (int i = 0; i <= num; i++) {
+            for (int k = 0; k <= i; k++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    /// from google
+    public static void makeAFullPyramid(int num) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
 }
+
