8dc3f874dc333aa49f4da24e2a32416a952a7f6d
alelignqa <alent.qa@gmail.com>
2021-04-14 10:09:09 -0400

    adding remainder

--- JavaPractice/src/easylevel/remainder/Negash.java
+++ JavaPractice/src/easylevel/remainder/Negash.java
@@ -1,4 +1,17 @@
 package easylevel.remainder;
 
 public class Negash {
+
+    public static void main(String[] args) {
+//        Negash.remainder(10,7);
+
+
+    }
+
+    public static void remainder(int numOne, int numTwo) {
+        if (numOne > numTwo) {
+            int remainder = numOne % numTwo;
+            System.out.println(remainder);
+        }
+    }
 }
