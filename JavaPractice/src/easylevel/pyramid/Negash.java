7334b48b88b40d3fc5088efa8e91966e249004a7
alelignqa <alent.qa@gmail.com>
2021-04-14 10:18:27 -0400

    making some changes on My Pyramid

--- JavaPractice/src/easylevel/pyramid/Negash.java
+++ JavaPractice/src/easylevel/pyramid/Negash.java
@@ -4,31 +4,32 @@ public class Negash {
 
     public static void main(String[] args) {
 //        Negash.mytrial();
-//        Negash.fullPyramid();
+        Negash.fullPyramid(10);
 
     }
+
     // this is what I have within 10mnts window
-    public static void mytrial(){
-        for (int i = 0;i<6;i++){
-            for (int j=0;j<i;j++){
+    public static void myTrial(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" * ");
             }
             System.out.println();
         }
-        for (int i = 0;i<6;i++){
-            for (int j=6;j>i;j--){
+        for (int i = 0; i < height; i++) {
+            for (int j = height; j > i; j--) {
                 System.out.print(" * ");
             }
             System.out.println();
         }
     }
 
-    public static void fullPyramid(){
-        for (int i = 0;i<=5;i++){
-            for (int j = 5;j >= i;j--){
+    public static void fullPyramid(int howTall) {
+        for (int i = 0; i <= howTall; i++) {
+            for (int j = howTall; j >= i; j--) {
                 System.out.print(" ");
             }
-            for (int n=0;n<=i;n++){
+            for (int n = 0; n <= i; n++) {
                 System.out.print(" *");
             }
             System.out.println();
