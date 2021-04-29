b6c78d2d671cbc2288826c2606fa6cb373e862c9
eashaarap <easha2272@gmail.com>
2021-04-29 17:37:45 -0400

    annoying

--- JavaPractice/src/popularjavainterviewquestions/iteratearraylist/Easha.java
+++ JavaPractice/src/popularjavainterviewquestions/iteratearraylist/Easha.java
@@ -14,7 +14,8 @@ public class Easha {
     }
 
     //for each
-    public static void arraylistIterateUsingForEach(){
+
+    public static void arraylistIterateUsingForEach() {
         ArrayList<String> countries = new ArrayList();
         countries.add("Bangladesh");
         countries.add("USA");
@@ -24,12 +25,13 @@ public class Easha {
         countries.add("Bolvia");
 
 
-        for(String cname: countries){
+        for (String cname : countries) {
             System.out.println(cname);
         }
     }
-    public static void arraylistIterateUsingForLoop(){
-        ArrayList <Integer> numbers = new ArrayList<>();
+
+    public static void arraylistIterateUsingForLoop() {
+        ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(0);
         numbers.add(1);
         numbers.add(2);
@@ -37,27 +39,27 @@ public class Easha {
         numbers.add(4);
         numbers.add(5);
 
-        for(int i =0; i < numbers.size(); i++){
+        for (int i = 0; i < numbers.size(); i++) {
             System.out.println(numbers.get(i));
         }
 
     }
 
-    public static void arrayListIterateUsingWhileLoop(){
-        ArrayList <String> friends = new ArrayList<>();
+    public static void arrayListIterateUsingWhileLoop() {
+        ArrayList<String> friends = new ArrayList<>();
         friends.add("Reto");
         friends.add("Negash");
         friends.add("Simar");
 
 
-        int myindex =0;
-        while(friends.size() > myindex){
+        int myindex = 0;
+        while (friends.size() > myindex) {
             System.out.println(friends.get(myindex));
-                myindex++;
+            myindex++;
         }
     }
 
-    public static void practice(){
+    public static void practice() {
         ArrayList<String> mylist = new ArrayList<>();
         mylist.add("Pineapple");
         mylist.add("Apple");
@@ -65,7 +67,7 @@ public class Easha {
         mylist.add("Monkey");
 
         int index = 0;
-        while(mylist.size() > index){
+        while (mylist.size() > index) {
             System.out.println(mylist.get(index));
             index++;
         }
